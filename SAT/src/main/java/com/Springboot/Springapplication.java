a802924f758ab26cac3ac63b7d111bde6b62ff66
koushik19990226 <60263783+koushik19990226@users.noreply.github.com>
2023-05-18 11:03:23 +0530

    Update Springapplication.java

--- SAT/src/main/java/com/Springboot/Springapplication.java
+++ SAT/src/main/java/com/Springboot/Springapplication.java
@@ -1,1 +1,14 @@
+package com.Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springapplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springapplication.class, args);
+	}
+
+}
 
